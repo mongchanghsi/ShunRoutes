116c7c2eaa5f4931b00939304bb8f8f6b537c322
mongchanghsi <m.changhsi@gmail.com>
2021-10-01 08:13:11 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Some React App</header>
+    </div>
+  );
+};
+
+export default App;
